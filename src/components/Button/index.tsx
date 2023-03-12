a779b76266f64a50f37e8f12f04b8ffb21445fe2
Rute Figueiredo <rute.figueiredo92@gmail.com>
2023-03-12 09:58:03 +0000

    refactored button to handle different background colors as props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,52 +1,34 @@
 import React, { ButtonHTMLAttributes } from 'react'
-import styled from 'styled-components'
-import { defaultTheme } from '../../styles/themes/default'
+import styled, { css } from 'styled-components'
 
 type Variant = 'normal' | 'small' | 'icon'
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: Variant
+  backgroundColor?: string
+  hoverColor?: string
 }
 
 export const Button: React.FC<Props> = ({
   variant = 'normal',
+  backgroundColor,
+  hoverColor,
   children,
   ...props
 }) => {
-  return <Button variant={variant} {...props}>{children}</Button>
+  return (
+    <StyledButton
+      variant={variant}
+      backgroundColor={backgroundColor}
+      hoverColor={hoverColor}
+      {...props}
+    >
+      {children}
+    </StyledButton>
+  )
 }
 
-const getPadding = (variant: Variant) => {
-  switch (variant) {
-    case 'small':
-      return {
-        background: defaultTheme['base-button'],
-        hoverBackground: defaultTheme['base-hover'],
-        textColor: defaultTheme['base-text'],
-        fontSize: '0.75rem',
-        padding: '0px 8px',
-      }
-    case 'icon':
-      return {
-        background: defaultTheme['purple-dark'],
-        hoverBackground: defaultTheme['purple'],
-        textColor: defaultTheme['white'],
-        padding: '8px',
-      }
-    default:
-      return {
-        background: defaultTheme['yellow'],
-        hoverBackground: defaultTheme['yellow-dark'],
-        textColor: defaultTheme['white'],
-        fontSize: '0.875rem',
-        padding: '12px 8px',
-      }
-  }
-}
-
-export const StyledButton = styled(Button).attrs((props) => ({
-  ...getPadding(props.variant ?? 'normal'),
-}))`
+const StyledButton = styled.button<Props>`
   display: flex;
   justify-content: center;
   align-items: center;
@@ -57,16 +39,63 @@ export const StyledButton = styled(Button).attrs((props) => ({
   line-height: 160%;
   text-transform: uppercase;
 
-  padding: ${(props) => props.padding};
-
-  background: ${(props) => props.background};
-
-  color: ${(props) => props.textColor};
-  font-size: ${(props) => props.fontSize};
-
   transition: all 0.2s ease;
 
+  ${(props) => {
+    const { variant, backgroundColor, hoverColor } = props
+
+    const normalStyle = css`
+      padding: 12px 8px;
+      font-size: 0.875rem;
+      background: ${backgroundColor ?? props.theme['yellow']};
+      color: ${props.theme['white']};
+    `
+
+    const iconStyle = css`
+      padding: 8px;
+      background: ${backgroundColor ?? props.theme['purple-dark']};
+      color: ${props.theme['white']};
+    `
+
+    const smallStyle = css`
+      padding: 0px 8px;
+      font-size: 0.75rem;
+      background: ${backgroundColor ?? props.theme['base-button']};
+      color: ${props.theme['base-text']};
+    `
+
+    if (variant === 'icon') {
+      return iconStyle
+    } else if (variant === 'small') {
+      return smallStyle
+    } else {
+      return normalStyle
+    }
+  }}
+
   &:hover {
-    background: ${(props) => props.hoverBackground};
+    ${(props) => {
+      const { variant, hoverColor } = props
+
+      const normalStyle = css`
+        background: ${hoverColor ?? props.theme['yellow-dark']};
+      `
+
+      const iconStyle = css`
+        background: ${hoverColor ?? props.theme['purple']};
+      `
+
+      const smallStyle = css`
+        background: ${hoverColor ?? props.theme['base-hover']};
+      `
+
+      if (variant === 'icon') {
+        return iconStyle
+      } else if (variant === 'small') {
+        return smallStyle
+      } else {
+        return normalStyle
+      }
+    }}
   }
 `
