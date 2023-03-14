72ad56fd87407a452148a5c37b92c4147002840d
Rute Figueiredo <rute.figueiredo92@gmail.com>
2023-03-14 23:01:02 +0000

    added react router dom and started to create routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Home } from './pages/Home'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router'
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={defaultTheme}>
         <Home />
+        <RouterProvider router={router} />
         <GlobalStyle />
       </ThemeProvider>
     </div>
